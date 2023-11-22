8598c1563e0004e3377ce668fc618b1addd27395
freemode <freemode1614@outlook.com>
2023-11-23 00:13:46 +0800

    chore

--- packages/plugin/src/index.ts
+++ packages/plugin/src/index.ts
@@ -16,32 +16,32 @@ export type TPluginManagerObj<T> = {
 export default function createPlugin<
   T extends PluginContext,
 >(): TPluginManagerObj<T> {
-  const appliedPlugins: TPluginHandlerObj<T>[] = [];
-  const plugins: TPluginHandlerObj<T>[] = [];
+    const appliedPlugins: TPluginHandlerObj<T>[] = [];
+    const plugins: TPluginHandlerObj<T>[] = [];
 
-  const add = (handler: TPluginHandlerObj<T>) => {
-    plugins.push(handler);
-  };
+    const add = (handler: TPluginHandlerObj<T>) => {
+        plugins.push(handler);
+    };
 
-  const apply = async (ctx: T) => {
-    if (plugins.length > 0) {
-      for await (const plugin of plugins) {
-        try {
-          if (appliedPlugins.includes(plugin)) {
-            console.warn(`Same plugin executed twice.`);
-          } else {
-            const ret = await plugin.handler(ctx);
-            appliedPlugins.push(plugin);
-          }
-        } catch (error) {
-          console.error("error", error);
+    const apply = async (ctx: T) => {
+        if (plugins.length > 0) {
+            for await (const plugin of plugins) {
+                try {
+                    if (appliedPlugins.includes(plugin)) {
+                        console.warn('Same plugin executed twice.');
+                    } else {
+                        const ret = await plugin.handler(ctx);
+                        appliedPlugins.push(plugin);
+                    }
+                } catch (error) {
+                    console.error('error', error);
+                }
+            }
         }
-      }
-    }
-  };
+    };
 
-  return {
-    add,
-    apply,
-  };
+    return {
+        add,
+        apply,
+    };
 }
