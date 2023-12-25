1c8eaa8f3400ef769d03d4002488675616f3a8d8
张三01 zhangsan <freemode1614@outlook.com>
2023-12-25 02:17:55 +0000

    remove require-local

--- packages/require-local/lib/index.d.ts
+++
@@ -1,2 +0,0 @@
-export declare function resolveLocalSync<T = unknown>(from: string, moduleId: string): T;
-export declare function resolveLocal<T = unknown>(from: string, moduleId: string): Promise<T | undefined>;
