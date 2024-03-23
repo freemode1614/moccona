c379b847f187274eab738e33d6a6de7f7bc1eb90
freemode <freemode1614@outlook.com>
2024-03-24 00:18:42 +0800

    Add prettier shareable configuration

--- packages/eslint-config/src/overrides/jest.ts
+++ packages/eslint-config/src/overrides/jest.ts
@@ -33,7 +33,8 @@ export default {
         ecmaVersion: 'latest',
         sourceType: 'module',
         ecmaFeatures: {
-            jsx: true, experimentalObjectRestSpread: true,
+            jsx: true,
+            experimentalObjectRestSpread: true,
         },
     },
     settings: {
